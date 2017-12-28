3a02396fb035b4b388a45b34a8e2cfb037b9c33d
Paul Mayer <paul@paul-mayer.eu>
2017-12-28 19:44:46 +0100

    all csv import fertig

--- test.go
+++ test.go
@@ -8,8 +8,8 @@ import (
 	"encoding/csv"
 	"strings"
 	"io/ioutil"
-	"fmt"
 	"io"
+	"fmt"
 )
 
 func check(e error) {
@@ -31,32 +31,106 @@ func main() {
 
 
 	//sqlStmt := "create table foo (id integer not null primary key, name text);"
-	sqlStmt := "CREATE TABLE t (tnr TEXT NOT NULL PRIMARY KEY, tname TEXT, farbe TEXT, gewicht INTEGER);"
+	sqlStmt := "CREATE TABLE T (tnr TEXT NOT NULL PRIMARY KEY, tname TEXT, farbe TEXT, gewicht INTEGER);" +
+		"CREATE TABLE L (lnr TEXT NOT NULL PRIMARY KEY, lname TEXT, status INTEGER, ort TEXT);" +
+		"CREATE TABLE P (pnr TEXT NOT NULL PRIMARY KEY, pname TEXT, ort TEXT);" +
+		"CREATE TABLE LTP (tnr TEXT, lnr TEXT, pnr TEXT, menge INTEGER, primary key (tnr,lnr,pnr));"
+
 	_, err = db.Exec(sqlStmt)
 	check(err)
 
-	dat, err := ioutil.ReadFile("./csv/T.csv")
+	data_t, err := ioutil.ReadFile("./csv/T.csv")
 	check(err)
 	//fmt.Print(string(dat))
 
-	r := csv.NewReader(strings.NewReader(string(dat)))
+	r_t := csv.NewReader(strings.NewReader(string(data_t)))
 
 	for {
-		record, err := r.Read()
+		record, err := r_t.Read()
 		if err == io.EOF {
 			break
 		}
 		check(err)
 
 		//fmt.Println(reflect.TypeOf(record))
-		//fmt.Println("RecordRow ", record)
+		fmt.Println("RecordRow ", record)
 
-		stmt, err := db.Prepare("INSERT INTO t(tnr) VALUES(?)")
+		stmt, err := db.Prepare("INSERT INTO t(tnr,tname,farbe,gewicht) VALUES(?,?,?,?)")
 		check(err)
 
-		stmt.Exec(record[0])
+		stmt.Exec(record[0], record[1], record[2], record[3])
 
 	}
+
+	data_l, err := ioutil.ReadFile("./csv/L.csv")
+	check(err)
+	//fmt.Print(string(dat))
+
+	l_t := csv.NewReader(strings.NewReader(string(data_l)))
+
+	for {
+		record, err := l_t.Read()
+		if err == io.EOF {
+			break
+		}
+		check(err)
+
+		//fmt.Println(reflect.TypeOf(record))
+		fmt.Println("RecordRow ", record)
+
+		stmt, err := db.Prepare("INSERT INTO l(lnr,lname,status,ort) VALUES(?,?,?,?)")
+		check(err)
+
+		stmt.Exec(record[0], record[1], record[2], record[3])
+
+	}
+
+	data_p, err := ioutil.ReadFile("./csv/P.csv")
+	check(err)
+	//fmt.Print(string(dat))
+
+	r_p := csv.NewReader(strings.NewReader(string(data_p)))
+
+	for {
+		record, err := r_p.Read()
+		if err == io.EOF {
+			break
+		}
+		check(err)
+
+		//fmt.Println(reflect.TypeOf(record))
+		fmt.Println("RecordRow ", record)
+
+		stmt, err := db.Prepare("INSERT INTO p(pnr,pname,ort) VALUES(?,?,?)")
+		check(err)
+
+		stmt.Exec(record[0], record[1], record[2])
+
+	}
+
+	data_ltp, err := ioutil.ReadFile("./csv/LTP.csv")
+	check(err)
+	//fmt.Print(string(dat))
+
+	r_ltp := csv.NewReader(strings.NewReader(string(data_ltp)))
+
+	for {
+		record, err := r_ltp.Read()
+		if err == io.EOF {
+			break
+		}
+		check(err)
+
+		//fmt.Println(reflect.TypeOf(record))
+		fmt.Println("RecordRow ", record)
+
+		stmt, err := db.Prepare("INSERT INTO ltp(tnr,lnr,pnr,menge) VALUES(?,?,?,?)")
+		check(err)
+
+		stmt.Exec(record[0], record[1], record[2], record[3])
+
+	}
+
 
 
 
